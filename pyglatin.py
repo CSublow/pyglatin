f9c42a0a9263c9e13a0a9a5920972210caf75d78
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-12-22 13:12:39 +0000

    Created README, moved docstring at the top of pyglatin.py to README

--- pyglatin.py
+++ pyglatin.py
@@ -1,7 +1,3 @@
-'''Pig Latin is an argot in which words in English are altered to conceal the words from others not familiar with the rules
-You convert a word to pig latin by transferring the initial consonant or consonant cluster of the word to the end of the word with -ay appended to the end.
-If the word starts with a vowel (including y), append -yay to the end. For example, "pig" would become igpay (taking the 'p' and 'ay' to create a suffix'''
-
 eng_pyg_translate = True
 
 while eng_pyg_translate == True:
