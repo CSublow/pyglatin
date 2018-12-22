37217205fdccfc0f44e065e2b87cc9a4d0871af1
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-12-22 16:17:28 +0000

    Added variable translate, wrapped main code block in while loop to check translate == true

--- pyglatin.py
+++ pyglatin.py
@@ -23,38 +23,43 @@ list_word = list(user_input)
 working_list_word = list(list_word)
 later_vowel = False
 
-# For each character in the inputted word
-for x in list_word:
-    # If a vowel is found first, we don't need to worry about any consonants. So we just convert the word into a string, add the suffix and print it, breaking from the loop
-    if x in vowels:
-        # If the vowel is found AFTER a consonant, we need to add the consonant suffix
-        if later_vowel == True:
-            pyg_word = ''.join(working_list_word).capitalize() + c_suffix
-            break
-        # If the vowel is the first character found, we want to add the vowel suffix
-        else:
-            pyg_word = ''.join(working_list_word).capitalize() + v_suffix
-            break
-    # If a consonant is found
-    elif x in consonants:
-        working_list_word.append(working_list_word[0])
-        working_list_word.pop(0)
-        counter += 1
-        # Once the counter is the same amount as the length of the word, this means that no vowels are in the word. So format the word and output it
-        if counter == len(working_list_word):
-            pyg_word = ''.join(working_list_word).capitalize() + c_suffix
-            break
-        later_vowel = True
+# Variable used for while loop, kept true as long as the user wants to translate words
+translate = True
 
-print(pyg_word)
+while translate == True:
+    # For each character in the inputted word
+    for x in list_word:
+        # If a vowel is found first, we don't need to worry about any consonants. So we just convert the word into a string, add the suffix and print it, breaking from the loop
+        if x in vowels:
+            # If the vowel is found AFTER a consonant, we need to add the consonant suffix
+            if later_vowel == True:
+                pyg_word = ''.join(working_list_word).capitalize() + c_suffix
+                break
+            # If the vowel is the first character found, we want to add the vowel suffix
+            else:
+                pyg_word = ''.join(working_list_word).capitalize() + v_suffix
+                break
+        # If a consonant is found
+        elif x in consonants:
+            working_list_word.append(working_list_word[0])
+            working_list_word.pop(0)
+            counter += 1
+            # Once the counter is the same amount as the length of the word, this means that no vowels are in the word. So format the word and output it
+            if counter == len(working_list_word):
+                pyg_word = ''.join(working_list_word).capitalize() + c_suffix
+                break
+            later_vowel = True
 
-# Exit the loop unless the user wants to translate another word
-print("Do you want to translate another word?")
-response = ''
-while response.lower() not in {"yes", "no"}:
-    response = input("Please enter yes or no: ")
-if response.lower() == "no":
-    print("Exiting program...")
-    eng_pyg_translate = False
+    print(pyg_word)
+
+    # Exit the loop unless the user wants to translate another word
+    print("Do you want to translate another word?")
+    response = ''
+    while response.lower() not in {"yes", "no"}:
+        response = input("Please enter yes or no: ")
+    if response.lower() == "no":
+        print("Exiting program...")
+        # Break out of the loop
+        eng_pyg_translate = False
 
 
